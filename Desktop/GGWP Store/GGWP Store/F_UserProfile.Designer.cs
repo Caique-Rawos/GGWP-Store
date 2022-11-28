b0d3436ccb42335f92383daa55fe4a28bed28e77
Caique Rawos <caique.rawos@gmail.com>
2022-11-27 23:18:43 -0300

    corrigindo perfil do Desktop

--- Desktop/GGWP Store/GGWP Store/F_UserProfile.Designer.cs
+++ Desktop/GGWP Store/GGWP Store/F_UserProfile.Designer.cs
@@ -33,10 +33,14 @@ namespace GGWP_Store
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.label4 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,12 +50,12 @@ namespace GGWP_Store
             this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(118, 216);
+            this.label2.Location = new System.Drawing.Point(131, 213);
             this.label2.Name = "label2";
             this.label2.Padding = new System.Windows.Forms.Padding(5);
-            this.label2.Size = new System.Drawing.Size(208, 41);
+            this.label2.Size = new System.Drawing.Size(228, 41);
             this.label2.TabIndex = 8;
-            this.label2.Text = "@GGWP Store";
+            this.label2.Text = "@Caique Rawos";
             // 
             // label3
             // 
@@ -59,45 +63,36 @@ namespace GGWP_Store
             this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Location = new System.Drawing.Point(13, 0);
             this.label3.Name = "label3";
             this.label3.Padding = new System.Windows.Forms.Padding(5, 5, 50, 5);
-            this.label3.Size = new System.Drawing.Size(576, 130);
+            this.label3.Size = new System.Drawing.Size(344, 34);
             this.label3.TabIndex = 10;
-            this.label3.Text = "Sobre nós:\r\nLoja virtual voltada ao público geek/gamer.\r\n\r\nFocada em trazer a mel" +
-    "hor experiência para nossos clientes.\r\n ";
+            this.label3.Text = "Boas-Vindas à Família Gamer! 🎮";
             // 
             // panel4
             // 
             this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.panel4.Controls.Add(this.label8);
+            this.panel4.Controls.Add(this.label7);
+            this.panel4.Controls.Add(this.label6);
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Controls.Add(this.label1);
             this.panel4.Controls.Add(this.label3);
-            this.panel4.Location = new System.Drawing.Point(26, 322);
+            this.panel4.Location = new System.Drawing.Point(25, 337);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(880, 267);
             this.panel4.TabIndex = 11;
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(118, 282);
-            this.label4.Name = "label4";
-            this.label4.Padding = new System.Windows.Forms.Padding(5, 5, 50, 5);
-            this.label4.Size = new System.Drawing.Size(421, 34);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Loja virtual voltada ao público geek/gamer.";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.Transparent;
-            this.panel2.BackgroundImage = global::GGWP_Store.Properties.Resources.Ellipse_20;
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
             this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel2.Location = new System.Drawing.Point(12, 216);
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Location = new System.Drawing.Point(25, 219);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(100, 100);
             this.panel2.TabIndex = 5;
@@ -105,7 +100,7 @@ namespace GGWP_Store
             // panel3
             // 
             this.panel3.BackgroundImage = global::GGWP_Store.Properties.Resources.Star_18;
-            this.panel3.Location = new System.Drawing.Point(332, 216);
+            this.panel3.Location = new System.Drawing.Point(142, 257);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(240, 44);
             this.panel3.TabIndex = 6;
@@ -121,6 +116,73 @@ namespace GGWP_Store
             this.panel1.Size = new System.Drawing.Size(969, 210);
             this.panel1.TabIndex = 0;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(13, 45);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(5, 5, 50, 5);
+            this.label1.Size = new System.Drawing.Size(889, 34);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Somos uma revenda de games e acessórios especializada, que está sempre pronta pra" +
+    " te atender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(13, 79);
+            this.label5.Name = "label5";
+            this.label5.Padding = new System.Windows.Forms.Padding(5, 5, 50, 5);
+            this.label5.Size = new System.Drawing.Size(413, 34);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "com toda atenção e disposição possíveis.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(13, 132);
+            this.label6.Name = "label6";
+            this.label6.Padding = new System.Windows.Forms.Padding(5, 5, 50, 5);
+            this.label6.Size = new System.Drawing.Size(880, 34);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Aqui também somos gamers de verdade e ficamos extremamente felizes em poder fazer" +
+    " parte da";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(13, 166);
+            this.label7.Name = "label7";
+            this.label7.Padding = new System.Windows.Forms.Padding(5, 5, 50, 5);
+            this.label7.Size = new System.Drawing.Size(603, 34);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "sua gameplay. Então, conte com a gente sempre que precisar :)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(13, 222);
+            this.label8.Name = "label8";
+            this.label8.Padding = new System.Windows.Forms.Padding(5, 5, 50, 5);
+            this.label8.Size = new System.Drawing.Size(335, 34);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Uma vez gamer, sempre gamer!";
+            // 
             // F_UserProfile
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
@@ -128,7 +190,6 @@ namespace GGWP_Store
             this.ClientSize = new System.Drawing.Size(1202, 616);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel4);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
@@ -151,6 +212,10 @@ namespace GGWP_Store
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
     }
 }
